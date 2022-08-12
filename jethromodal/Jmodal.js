b8c06e775872cb39fba464061eed1974f762f4aa
Olawole Jethro <kunlekzi2@gmail.com>
2022-08-12 13:49:20 +0100

    practise jmodal project

---
+++ jethromodal/Jmodal.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// console.log(btnsOpenModal);
+
+// btnCloseModal.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+// let open = close;
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
+}
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//  if (open === close) {
+//     btnsOpenModal[i].addEventListener('click', openModal);
+//     btnCloseModal.addEventListener('click', closeModal);
+//     overlay.addEventListener('click', closeModal);
+//   }
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    // if (!modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+// });
